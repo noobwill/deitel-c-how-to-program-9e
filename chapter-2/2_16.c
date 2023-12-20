522ef91cb27d9a37ccc70be03b2b5d7bfebc5cd6
chillwillyo <52179342+chillwillyo@users.noreply.github.com>
2023-12-20 14:47:55 -0400

    Removed space before left curly brace after main

--- chapter-2/2_16.c
+++ chapter-2/2_16.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void){
 	int x;
 	int y;
 	printf("%s", "Enter two integers: ");
--- chapter-2/2_17.c
+++ chapter-2/2_17.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int main(void) {
+int main(void){
 	printf("1 2 3 4\n");
 	printf("%d %d %d %d\n", 1, 2, 3, 4);
 	printf("1 ");
