8b5e1b8e4afffb2e72022ee895c2c40cb558524b
chillwillyo <52179342+chillwillyo@users.noreply.github.com>
2023-12-20 14:21:44 -0400

    Exercises 2.16 and 2.17

---
+++ chapter-2/2_16.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	int x;
+	int y;
+	printf("%s", "Enter two integers: ");
+	scanf("%d%d", &x, &y);
+	printf("Sum of x and y is %d\n", x + y);
+	printf("Product of x and y is %d\n", x * y);
+	printf("Difference of x and y is %d\n", x - y);
+	printf("Quotient of x and y is %d\n", x / y);
+	printf("Remainder of x / y is %d\n", x % y);
+}
