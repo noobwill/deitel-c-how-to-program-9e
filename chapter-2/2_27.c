37bcff40af2ae9c6f84e79fb4b0ed4738a2ab606
chillwillyo <52179342+chillwillyo@users.noreply.github.com>
2023-12-21 08:59:23 -0400

    Modified comments and added return statement

--- chapter-2/2_27.c
+++ chapter-2/2_27.c
@@ -3,17 +3,17 @@
 * Write a program that calculates the squares and cubes of the numbers from 0 to 10 and uses tabs to display the following tables of values:
 * 
 * number	square	cube
-* 0				0				0
-* 1				1				1
-* 2				4				8
-* 3				9				27
-* 4				16			64
-* 5				25			125
-* 6				36			216
-*	7				49			343
-*	8				64			512
-* 9				81			729
-* 10			100			1000
+* 0			0		0
+* 1			1		1
+* 2			4		8
+* 3			9		27
+* 4			16		64
+* 5			25		125
+* 6			36		216
+* 7			49		343
+* 8			64		512
+* 9			81		729
+* 10		100		1000
 */
 
 #include <stdio.h>
@@ -31,4 +31,6 @@ int main(void){
 	printf("8\t%d\t%d\n", 8 * 8, 8 * 8 * 8);
 	printf("9\t%d\t%d\n", 9 * 9, 9 * 9 * 9);
 	printf("10\t%d\t%d\n", 10 * 10, 10 * 10 * 10);
+
+	return 0;
 }
